9278054be696520c5fbde3c056cb2067f31a5b42
Viacheslav Khrykin <vh78aks@gmail.com>
2022-04-17 14:23:01 +0300

    Prepare screen pages. Add worker edit layout

--- app/src/main/java/com/example/workschedule/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/workschedule/ui/main/MainFragment.kt
@@ -1,4 +1,4 @@
-package com.example.workschedule.ui.gallery
+package com.example.workschedule.ui.main
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,15 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.workschedule.R
-import com.example.workschedule.databinding.FragmentGalleryBinding
+import com.example.workschedule.databinding.FragmentMainBinding
 
-class GalleryFragment : Fragment() {
+class MainFragment : Fragment() {
 
-    private lateinit var galleryViewModel: GalleryViewModel
-    private var _binding: FragmentGalleryBinding? = null
+    private lateinit var mainViewModel: MainViewModel
+    private var _binding: FragmentMainBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,17 +22,17 @@ class GalleryFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+    ): View {
+        mainViewModel =
+            ViewModelProvider(this)[MainViewModel::class.java]
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
+        val textView: TextView = binding.textHome
+        mainViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
-        })
+        }
         return root
     }
 
---
+++ app/src/main/java/com/example/workschedule/ui/worker_edit/WorkerEditFragment.kt
@@ -0,0 +1,41 @@
+package com.example.workschedule.ui.worker_edit
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.workschedule.databinding.FragmentWorkerEditBinding
+import com.example.workschedule.ui.main.WorkerEditViewModel
+
+class WorkerEditFragment : Fragment() {
+
+    private lateinit var workerEditViewModel: WorkerEditViewModel
+    private var _binding: FragmentWorkerEditBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        workerEditViewModel =
+            ViewModelProvider(this)[WorkerEditViewModel::class.java]
+
+        _binding = FragmentWorkerEditBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        workerEditViewModel.text.observe(viewLifecycleOwner) {
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

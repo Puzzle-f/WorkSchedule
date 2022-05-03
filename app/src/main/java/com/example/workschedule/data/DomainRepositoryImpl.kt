2982176b09d150128a08971d41cb5b9c9c5720b6
konstantin <kosvasl09@gmail.com>
2022-05-03 20:24:23 +0900

    подправлен DomainRepositoryImpl

--- app/src/main/java/com/example/workschedule/data/DomainRepositoryImpl.kt
+++ app/src/main/java/com/example/workschedule/data/DomainRepositoryImpl.kt
@@ -1,13 +1,52 @@
 package com.example.workschedule.data
 
 import com.example.workschedule.domain.DomainRepository
-import com.example.workschedule.domain.domainpersonmodel.DomainPersonModel
+import com.example.workschedule.domain.models.DomainDriverModel
+import com.example.workschedule.domain.models.DomainTrainModel
+import com.example.workschedule.domain.models.DomainTrainRunModel
+import kotlinx.coroutines.flow.Flow
 
 class DomainRepositoryImpl(
     private val databaseRepository: DatabaseRepository
 ) : DomainRepository {
 
-    override fun saveNewPerson(personModel: DomainPersonModel) {
-        databaseRepository.saveNewPerson()
+    override fun getAllTrainsRunList(): Flow<List<DomainTrainRunModel>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTrainRun(trainRunId: Int): DomainTrainRunModel {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllTrainsList(): List<DomainTrainModel> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllDriversList(): List<DomainDriverModel> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun saveTrainRun(trainRunModel: DomainTrainRunModel) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getTrainRunListForDriver(driverId: Int): Flow<List<DomainTrainRunModel>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getDriver(driverId: Int): DomainDriverModel {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun saveDriver(domainDriverModel: DomainDriverModel) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTrain(trainNumber: Int): DomainTrainModel {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun saveTrain(domainTrainModel: DomainTrainModel) {
+        TODO("Not yet implemented")
     }
 }

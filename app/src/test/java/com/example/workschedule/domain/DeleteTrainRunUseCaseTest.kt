d1fd311d8c1feb0bf3cc7ac284c86497718f6631
konstantin <kosvasl09@gmail.com>
2022-05-22 19:22:15 +0900

    тестирование UseCase'ов с возвращаемым типом Unit

---
+++ app/src/test/java/com/example/workschedule/domain/DeleteTrainRunUseCaseTest.kt
@@ -0,0 +1,27 @@
+package com.example.workschedule.domain
+
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.mock
+
+class DeleteTrainRunUseCaseTest {
+
+    private val domainRepository = mock<DomainRepository>()
+
+    @After
+    fun tearDown() {
+        Mockito.reset(domainRepository)
+    }
+
+    @Test
+    fun should_invoke_one_time() {
+        val useCase = DeleteTrainRunUseCase(domainRepository)
+        val trainRunId = 1
+        runBlocking {
+            useCase.execute(trainRunId)
+            Mockito.verify(domainRepository, Mockito.times(1)).deleteTrainRun(trainRunId)
+        }
+    }
+}
---
+++ app/src/test/java/com/example/workschedule/domain/SaveTrainUseCaseTest.kt
@@ -0,0 +1,28 @@
+package com.example.workschedule.domain
+
+import com.example.workschedule.domain.models.Train
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.mock
+
+class SaveTrainUseCaseTest {
+
+    private val domainRepository = mock<DomainRepository>()
+
+    @After
+    fun tearDown() {
+        Mockito.reset(domainRepository)
+    }
+
+    @Test
+    fun should_invoke_one_time() {
+        val useCase = SaveTrainUseCase(domainRepository)
+        val train = Train(id = 1, direction = "Moscow")
+        runBlocking {
+            useCase.execute(train)
+            Mockito.verify(domainRepository, Mockito.times(1)).saveTrain(train)
+        }
+    }
+}

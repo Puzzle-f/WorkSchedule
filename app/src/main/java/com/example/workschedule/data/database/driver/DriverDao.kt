086e2681954e9e58c950dd07d08e77f4a2512ae5
Viacheslav Khrykin <vh78aks@gmail.com>
2022-06-12 16:08:40 +0300

    Add ordering for reading DB data

--- app/src/main/java/com/example/workschedule/data/database/driver/DriverDao.kt
+++ app/src/main/java/com/example/workschedule/data/database/driver/DriverDao.kt
@@ -8,7 +8,7 @@ import androidx.room.Query
 @Dao
 interface DriverDao {
     // Получить весь список машинистов
-    @Query("SELECT * FROM DriverEntity")
+    @Query("SELECT * FROM DriverEntity ORDER BY surname")
     suspend fun getAllDrivers(): List<DriverEntity>
     // Получить машиниста по id
     @Query("SELECT * FROM DriverEntity WHERE id LIKE :driverId")
--- app/src/main/java/com/example/workschedule/data/database/train/TrainDao.kt
+++ app/src/main/java/com/example/workschedule/data/database/train/TrainDao.kt
@@ -10,11 +10,8 @@ import androidx.room.Query
 @Dao
 interface TrainDao {
 //    Получить все поезда
-    @Query("SELECT * FROM TrainEntity")
+    @Query("SELECT * FROM TrainEntity ORDER BY direction")
     suspend fun getAllTrains(): List<TrainEntity>
-//    Получить все поезда на указанную дату
-//    @Query("SELECT * FROM TrainEntity WHERE start LIKE :start")
-//    suspend fun getTrainByDataStart(start: GregorianCalendar): List<TrainEntity>
 //    Получить поезд по номеру
     @Query("SELECT * FROM TrainEntity WHERE id LIKE :trainId")
     suspend fun getTrainById(trainId: Int): TrainEntity
